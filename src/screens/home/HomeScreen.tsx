d0892cafa6d8e2af23a1f6809136bcd3d8cb090d
Kevin-Jhonatan <kevin7.world@gmail.com>
2024-09-08 22:40:16 -0400

    Add config path aliases with TypeScript

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import {Text, View} from 'react-native';
 import Car from '../../assets/icons/home/car.svg';
 
 export const HomeScreen = () => {
   return (
     <View>
-      <Text>Home Screen User</Text>
+      <Text>Home Screen User Kev</Text>
       <Car width={120} height={40} />
     </View>
   );
