807b868c7b82c86ed25071a994ba3876412eab63
Kevin-Jhonatan <kevin7.world@gmail.com>
2024-09-08 23:27:04 -0400

    Implement twrnc with tailwind

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import Car from '../../assets/icons/home/car.svg';
+import tw from 'twrnc';
 
 export const HomeScreen = () => {
   return (
-    <View>
-      <Text>Home Screen User Kev</Text>
+    <View style={tw`flex-1 justify-center items-center bg-blue-500`}>
+      <Text style={tw`text-black text-lg`}>Home Screen User Kev</Text>
       <Car width={120} height={40} />
     </View>
   );
